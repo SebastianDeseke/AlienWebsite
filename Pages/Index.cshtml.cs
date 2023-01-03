41574d5c1e6f160c34b8a115c757eb7ae348a8e0
Sebastian Deseke <deseke97@gmail.com>
2023-01-03 10:22:28 +0100

    First commit for website

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SebsFirstWebapp.Pages;
+
+public class IndexModel : PageModel
+{
+    [Parameter]
+    public string Name { get; set; }
+    [Parameter]
+    public string Time { get; set; }
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        Name = "Human Being";
+        Time = DateTime.Now.ToString();
+        _logger.LogWarning("This is a warning message");
+    }
+}
